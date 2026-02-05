6d3695ffef1df20080fd60b164dc858e0b354f88
Saswat <mohantysaswat154@gmail.com>
2026-02-05 10:28:18 +0530

    outputting the custom component in the browser

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+export default function MeetupDetails() {
+  return (
+    <MeetupDetail
+      title="A First Meetup"
+      image="https://images.squarespace-cdn.com/content/v1/603709c4fe10c7013cc59c86/1643927361362-062ORYR7Q698R0GS4KOP/unsplash-image-PTRzqc_h1r4.jpg"
+      address="Some Address 5, 12345 Some City"
+      description="This is a first meetup."
+    />
+  );
+}
