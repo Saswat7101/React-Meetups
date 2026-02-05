84db492727bc82a6c25898a92f23bb2bea5a2a18
Saswat <mohantysaswat154@gmail.com>
2026-02-05 09:40:51 +0530

    outputting a list of meetups

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,24 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  {
+    id: "m1",
+    title: "A First Meetup",
+    image:
+      "https://images.squarespace-cdn.com/content/v1/603709c4fe10c7013cc59c86/1643927361362-062ORYR7Q698R0GS4KOP/unsplash-image-PTRzqc_h1r4.jpg",
+    address: "Some Address 5, 12345 Some City",
+    description: "This is a first meetup",
+  },
+  {
+    id: "m2",
+    title: "A Second Meetup",
+    image:
+      "https://images.squarespace-cdn.com/content/v1/603709c4fe10c7013cc59c86/1643927361362-062ORYR7Q698R0GS4KOP/unsplash-image-PTRzqc_h1r4.jpg",
+    address: "Some Address 10, 12345 Some City",
+    description: "This is a second meetup",
+  },
+];
+
 export default function HomePage() {
-  return <h1>Home Page</h1>;
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
