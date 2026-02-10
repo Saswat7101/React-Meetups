7f67551d5c5edf3bdf4731cc7c1dce205f6d0c4f
Saswat <mohantysaswat154@gmail.com>
2026-02-10 09:42:39 +0530

    adding logic if we fetch data from a http request

--- components/meetups/MeetupList.js
+++ components/meetups/MeetupList.js
@@ -1,7 +1,11 @@
-import MeetupItem from './MeetupItem';
-import classes from './MeetupList.module.css';
+import MeetupItem from "./MeetupItem";
+import classes from "./MeetupList.module.css";
 
 function MeetupList(props) {
+  if (!props.meetups) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <ul className={classes.list}>
       {props.meetups.map((meetup) => (
