61b5d647353369277119e175663c9948b148c0b6
Saswat <mohantysaswat154@gmail.com>
2026-02-10 10:06:25 +0530

    working with params for SSG data fetching

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -10,3 +10,24 @@ export default function MeetupDetails() {
     />
   );
 }
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        id: meetupId,
+        title: "A First Meetup",
+        image:
+          "https://images.squarespace-cdn.com/content/v1/603709c4fe10c7013cc59c86/1643927361362-062ORYR7Q698R0GS4KOP/unsplash-image-PTRzqc_h1r4.jpg",
+        address: "Some Address 5, 12345 Some City",
+        description: "This is a first meetup",
+      },
+    },
+  };
+}
