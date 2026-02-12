d08c22a4954a9bfa1590fd3793feba43c6c43705
Saswat <mohantysaswat154@gmail.com>
2026-02-12 09:43:19 +0530

    setting up mongodb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,26 @@
-// api/new-meetup
+import { MongoClient } from "mongodb";
 
-function handler(req, res) {
+// api/new-meetup
+// POST/api/new-meetup
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = body;
+    const client = await MongoClient.connect(
+      "mongodb+srv://mohantysaswat154_db_user:oPNliTEYHDfCdUfe@cluster0.q9sb7tz.mongodb.net/meetups?appName=Cluster0",
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+    client.close();
+
+    res.status(201).json({
+      message: "Meetup Inserted!",
+    });
   }
 }
 
